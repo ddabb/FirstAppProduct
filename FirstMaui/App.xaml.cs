5faae63d5928ff2449eeedca7eaf3043f6c9bc97
60138546 <1011888891@qq.com>
2024-02-17 16:48:22 +0800

    first maui

---
+++ FirstMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FirstMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
